ee40c7fcea0d4a218e5e6e389483ce79c3cc32a6
Ali Gurmani <ceoalsido@gmail.com>
2022-05-29 02:39:17 +0500

    Arranged content in overlay component

--- src/components/OverlayDemo.vue
+++ src/components/OverlayDemo.vue
@@ -9,12 +9,7 @@
                 :modal="true">
           <p class="line-height-3 m-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit
-            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
           <template #footer>
             <Button label="Ok" @click="close" icon="pi pi-check" class="p-button-outlined"/>
