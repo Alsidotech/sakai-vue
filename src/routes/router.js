fa6b0a88877606f346fd122834ec916142445f91
Ali Gurmani <ceoalsido@gmail.com>
2022-05-29 03:20:22 +0500

    Removed hash from url

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import App from '../App.vue';
 
 const routes = [
@@ -169,7 +169,8 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
+    mode: 'history',
     routes,
 });
 
