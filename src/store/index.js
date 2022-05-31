1e4affd4fc9312ffaf41a05bc7e3a72e7f0d3bf9
Ali Gurmani <ceoalsido@gmail.com>
2022-05-31 13:49:25 +0500

    Setup Vuex State management and Created Stores

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vuex from "vuex";
+
+//Import other store modules
+import users from "./modules/users";
+
+// Create a new store instance.
+export default new Vuex.Store({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {users}
+})
---
+++ src/store/modules/users.js
@@ -0,0 +1,19 @@
+import {Resource} from "../../services/http-resource-service";
+
+export default {
+    namespaced: true,
+    state: {
+        currentUser: localStorage.getItem('test_user') ? JSON.parse(localStorage.getItem('test_user')) : null,
+        me: 'Ali Hassan',
+
+        registerReq: new Resource('/register', 'post'),
+        logInReq: new Resource('/login', 'post'),
+        logOutReq: new Resource('/logout', 'get'),
+    },
+    mutations: {
+        SET_CURRENT_USER: (state, payload) => {
+            state.currentUser = payload
+        },
+    },
+    actions: {},
+}
